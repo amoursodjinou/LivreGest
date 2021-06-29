d5893b0cc300e37b3d03cd86e24c5c9535155159
Amour SODJINOU <asodjnou@afytech.com>
2021-06-29 17:31:34 +0100

    Le projet meme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LivreGest';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyDPnD1b8iJCCEdQtChFfyticAvG85NJdD8",
+      authDomain: "livre-9e56d.firebaseapp.com",
+      projectId: "livre-9e56d",
+      storageBucket: "livre-9e56d.appspot.com",
+      messagingSenderId: "646587643731",
+      appId: "1:646587643731:web:4ff46cc5965e55bebb13a6",
+      measurementId: "G-KNKPPM77WT"
+    };
+    firebase.initializeApp(config);
+  }
 }
